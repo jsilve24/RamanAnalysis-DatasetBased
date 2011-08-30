65ddac16993a140000314ef5340fb051eed32087
Sasquach <jsilve24@gmail.com>
2011-08-30 19:48:02 -0400

    Finally Fixed Constructor Problem - Now input acts exactly as dataset method

--- ramandataset.m
+++ ramandataset.m
@@ -7,32 +7,13 @@ classdef ramandataset < dataset
     Apparatus
     end
     
-    methods
-        function spec = ramandataset(data,names)
+    methods        
+      function spec = ramandataset(varargin)
         %RAMANDATASET Class Constuctor
-        % INPUT DATA: spec = ramandataset(data,names);
-        % where data is a double matrix of the form   
-        % **********
-        % k ri k ri k ri
-        % **********
-        % and names is a 1 by n cell array of strings with the names  
+        % INPUT DATA: same way as if using the dataset class.      
+          spec = spec@dataset(varargin{:});      
+      end % END CONSTRUCTOR
         
-        if nargin == 0
-            super_args{1} = [0];
-            super_args{2} = '';
-        else
-           for i=1:size(data,2)/2 
-                input = [];
-                super_args{1} = data(:,2*i-1:2*i);
-                super_args{2} = names{i};
-                input = {input super_args};
-           end %END FOR
-        end %END IF
-        
-        spec = spec@dataset(input{:});
-        
-        
-        end % END CONSTRUCTOR
     end % END METHODS
     
     methods
--- ramandatasetDRIVER.m
+++ ramandatasetDRIVER.m
@@ -1,6 +1,8 @@
 % RAMANDATASETdriver
 
+clear classes;
 load TestData.mat;
 x = [sample_10_dried sample_11_dried sample_12_dried];
 names = {'sample10','sample11','sample12'};
-spec = ramandataset(x,name)
+% spec = ramandataset(x,name);
+% 
