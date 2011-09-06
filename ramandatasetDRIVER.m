aff9a1bb11d653461332a497a94678cd57366476
Sasquach <jsilve24@gmail.com>
2011-09-06 00:31:58 -0400

    RAMANDATASET v0.1
    +Fixed Dataset issue of not taking spectra of differing length.
    +Overloaded Arithmetic Operators for ramandataset Math +Overloaded
    Get/Set functions for ramandataset
    
    Conflicts:
    
    	ramandataset.m
    	ramandataset.m~
    	ramandatasetDRIVER.m
    
    Signed-off-by: Sasquach <jsilve24@gmail.com>

--- ramandatasetDRIVER.m
+++ ramandatasetDRIVER.m
@@ -2,5 +2,16 @@
 
 % clear classes;
 load TestData.mat;
+<<<<<<< HEAD
 spec = ramandataset({sample_7_dried,'sample_7_dried'},...
     {sample_8_dried,'sample_8_dried'},{sample_9_dried,'sample_9_dried'});
+=======
+% sample_7_dried = sample_7_dried(1:end-10,:);
+spec = ramandataset({sample_7_dried,'sample_7_dried'},...
+    {sample_8_dried,'sample_8_dried'},{sample_9_dried,'sample_9_dried'});
+
+% % Testing Overloaded Arithmetic Operators
+% disp(spec(4,1));
+% spec=spec./spec(:,1);
+% disp(spec(4,1));
+>>>>>>> 062a385... RAMANDATASET v0.1
